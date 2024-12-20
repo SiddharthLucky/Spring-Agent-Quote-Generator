2f103c18c3f5c9c643d5c59e9422e491d62208b8
SiddharthLucky <siddharth.luckyreddy@gmail.com>
2024-12-20 15:51:14 +0530

    Added spring project files, initial commit

---
+++ src/test/java/com/aiagents/quote_generator/QuoteGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aiagents.quote_generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuoteGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
